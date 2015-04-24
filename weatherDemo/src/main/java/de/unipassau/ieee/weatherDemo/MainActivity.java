5ce833951d136f6d8dd58101323c1185274537b4
Niko Fink <nfink95@gmail.com>
2015-04-25 00:08:25 +0200

    Add WeatherIntentService for updating weather information in the background

--- weatherDemo/src/main/java/de/unipassau/ieee/weatherDemo/MainActivity.java
+++ weatherDemo/src/main/java/de/unipassau/ieee/weatherDemo/MainActivity.java
@@ -1,19 +1,56 @@
 package de.unipassau.ieee.weatherDemo;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.Serializable;
+
+import static de.unipassau.ieee.weatherDemo.WeatherIntentService.INTENT_WEATHER_RESPONSE;
 
 
 public class MainActivity extends ActionBarActivity {
+    private final BroadcastReceiver weatherReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.hasExtra(WeatherIntentService.EXTRA_WEATHER_EXCEPTION)){
+                Exception exception = (Exception) intent.getSerializableExtra(WeatherIntentService.EXTRA_WEATHER_EXCEPTION);
+                exception.printStackTrace();
+                ((TextView) findViewById(R.id.weatherText)).setText(
+                        exception.toString()
+                );
+            }else {
+                ((TextView) findViewById(R.id.weatherText)).setText(
+                        intent.getStringExtra(WeatherIntentService.EXTRA_WEATHER_DATA)
+                );
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerReceiver(weatherReceiver, new IntentFilter(INTENT_WEATHER_RESPONSE));
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        WeatherIntentService.updateCityWeather(this, "Passau");
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(weatherReceiver);
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
